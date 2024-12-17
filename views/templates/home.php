bb933b30e640d0ca0af09f9978090975a7f6863f
Majdoline <majdolineez@gmail.com>
2024-12-17 12:05:01 +0100

    Mise à jour du layout

--- views/templates/home.php
+++ views/templates/home.php
@@ -4,25 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Spotifeux</title>
-    <link rel="stylesheet" href="assets/css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
-<!--HEADER-->
-    <header>
-        <div class="search">
-            <input type="text" placeholder=" Rechercher">
-            <button type="submit"><i class="fas fa-search"></i></button>
-
-        </div>
-    <div>
-        <a href="#"><i class="fas fa-home"></i></a>
-    </div>
-    <div>
-        <a href="#"><i class="fas fa-user"></i></a>
-    </div>
-    </header>
-    
+   <h1>BLABLABLA</h1>
 </body>
 </html>
 
