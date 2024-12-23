c351676aecfe6bc34dcedda9c914db9a44554c7e
Majdoline <majdolineez@gmail.com>
2024-12-23 10:56:14 +0100

    debug

--- views/templates/home.php
+++ views/templates/home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Spotifeux</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/home.css?v=1.6">
+    <link rel="stylesheet" href="assets/css/home.css?v=1.0">
 </head>
 
 <body>
